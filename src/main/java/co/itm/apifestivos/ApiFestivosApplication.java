d5e4d1d22505324788352bfd7ad781aaa791072a
Dan <daniielmartinez010@gmail.com>
2025-06-02 21:03:33 -0500

    Create ApiFestivosApplication.java

---
+++ src/main/java/co/itm/apifestivos/ApiFestivosApplication.java
@@ -0,0 +1,11 @@
+package co.itm.apifestivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFestivosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFestivosApplication.class, args);
+    }
+}
